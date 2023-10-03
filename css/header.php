042859642a515f2199c90f389b5c793474a699f4
shahzaib abid <shahzaibabidsaeed@gmail.com>
2023-10-03 16:38:35 +0500

    footer

---
+++ css/header.php
@@ -0,0 +1,107 @@
+<div class="site-top-2"> 
+         <header class="header nav-down" id="header-2"> 
+            <!-- <div class="container">
+               <div class="row"> -->
+                  <!-- <div class="logo-area clearfix">
+                     <div class="logo col-lg-3 col-md-12">
+                        <a href="index.html">
+                           <img src="images/rd_logo.png" alt="">
+                        </a>
+                     </div>
+                     <!- logo end-->
+                     <!-- <div class="col-lg-9 col-md-12 pull-right">
+                        <ul class="top-info unstyled">
+                           <!- <li><span class="info-icon"><i class="icon icon-phone3"></i></span>
+                              <div class="info-wrapper">
+                                 <p class="info-title">24/7 Response Time</p>
+                                 <p class="info-subtitle">(+44) 7456 44098</p>
+                              </div>
+                           </li> -->
+                           <!-- <li><span class="info-icon"><i class="icon icon-envelope"></i></span>
+                              <div class="info-wrapper">
+                                 <p class="info-title">Send Your Query</p>
+                                 <p class="info-subtitle">info@risingdigitally.com</p>
+                              </div>
+                           </li> -->
+                          
+                        <!-- </ul>
+                     </div>  -->
+                     <!-- Col End-->
+                  <!-- </div>  -->
+                 
+                  <!-- Logo Area End-->
+               <!-- </div> 
+            </div> -->
+             <!-- <hr> -->
+           
+            <!-- Container end-->
+            <div class="site-nav-inner site-navigation navigation navdown">
+               <div class="container">
+                  <nav class="navbar navbar-expand-lg ">
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"><i class="icon icon-menu"></i></span></button>
+                     <!-- End of Navbar toggler-->
+
+                     <a href="index.html">
+                        <img src="images/rd_logo.png" alt="">
+                     </a>
+
+                    
+                     <div class="collapse navbar-collapse justify-content-center" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                           <li class="nav-item dropdown active"><a class="nav-link" href="index.html">Home</a>
+                             
+                          
+                              
+                  
+                           <!-- li end-->
+                           <li class="nav-item dropdown">
+                                 <a href="about.html">About Us</a>
+                           </li>
+
+                               <li class="nav-item dropdown">
+                              <a class="nav-link" href="#" data-toggle="dropdown">
+                        Services
+                         <i class="fa fa-angle-down"></i>
+                       </a>
+                              <ul class="dropdown-menu" role="menu">
+                                 <li><a href="#">Influencer Marketing</a></li>
+                                 <li><a href="#">Social Media Management</a></li>
+                                 <li><a href="#">Graphic Designining</a></li>
+                                 <li><a href="#">Video Editing</a></li>
+                                 <li><a href="#">Facebook Ads</a></li>
+                                 <li><a href="#">Website Development</a></li>
+                              </ul>
+                           </li>
+                           <!-- li end-->
+
+                   
+
+                         <!-- li end-->
+                         <li class="nav-item dropdown">
+                           <a href="project-all.html">Portfolio</a>
+
+                           
+                         </li>
+
+                         <li class="nav-item dropdown">
+                           <a href="contact.html">Lets Talk</a>
+
+                           
+                         </li>
+                        </ul>
+                        <!--Nav ul end-->
+                     </div>
+                     <!-- <a href="contact.html" class="top-right-btn btn btn-primary">Let's Talk</a> -->
+                     <!-- Top bar btn -->
+                  </nav>
+                  <!-- Collapse end-->
+
+                  
+                  
+               </div>
+            </div>
+            <!-- Site nav inner end-->
+         </header>
+         <!-- Header end-->
+      </div>
